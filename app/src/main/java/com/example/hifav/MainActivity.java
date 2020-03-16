f45b7233763a85b172e5f8d6e28bbbdaef6d9584
Ajuogaaz <brianlinus1753@gmail.com>
2020-03-16 00:33:14 -0500

    ToolBar Addition

--- app/src/main/java/com/example/hifav/MainActivity.java
+++ app/src/main/java/com/example/hifav/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.hifav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolBar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("HiFav");
     }
+
 }
