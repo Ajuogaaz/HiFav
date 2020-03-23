5e9d307e254523577436f6bad59ad9f1d71d104f
Ajuogaaz <brianlinus1753@gmail.com>
2020-03-22 20:26:21 -0500

    Debug updates

--- app/src/main/java/com/example/hifav/MainActivity.java
+++ app/src/main/java/com/example/hifav/MainActivity.java
@@ -10,10 +10,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference Rootref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        Rootref = FirebaseDatabase.getInstance().getReference();
 
         mToolBar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolBar);
@@ -54,13 +62,40 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.i("we got in", "Current user is true");
             sendUserToLoginActivity();
+        }else
+        {
+            VerifyUserExistence();
         }
+    }
+
+    private void VerifyUserExistence() {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+        Rootref.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.child("name").exists()))
+                {
+                    Toast.makeText( MainActivity.this,
+                            getString(R.string.welcome2),
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    sendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void sendUserToLoginActivity()
     {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
 
     @Override
@@ -86,12 +121,19 @@ public class MainActivity extends AppCompatActivity {
 
         else if(item.getItemId() == R.id.main_settings_option)
         {
-
+            sendUserToSettingsActivity();
         }
         else if(item.getItemId() == R.id.main_find_friends_option)
         {
 
         }
         return true;
+    }
+    private void sendUserToSettingsActivity()
+    {
+        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(settingsIntent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/hifav/SettingsActivity.java
@@ -0,0 +1,108 @@
+package com.example.hifav;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Button UpdateAccountSettings;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage;
+
+    private String currentUserId;
+    private FirebaseAuth mAuth;
+    private DatabaseReference Rootref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        Rootref = FirebaseDatabase.getInstance().getReference();
+
+        InnitializeFields();
+        UpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdateSettings();
+            }
+        });
+    }
+
+    private void UpdateSettings() {
+        String setUserName = userName.getText().toString();
+        String setStatus = userStatus.getText().toString();
+
+        if(TextUtils.isEmpty(setUserName)){
+            Toast.makeText(SettingsActivity.this,
+                    "Please enter your username...",
+                    Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(setStatus)){
+            Toast.makeText(SettingsActivity.this,
+                    "Please enter your status...",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            HashMap<String, String> profileMap = new HashMap<>();
+            profileMap.put("uid", currentUserId);
+            profileMap.put("name", setUserName);
+            profileMap.put("status", setStatus);
+            Rootref.child("Users").child(currentUserId).setValue(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()) {
+                                sendUserToMainActivity();
+                                Toast.makeText(SettingsActivity.this,
+                                        "Profile updated succesfully",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingsActivity.this,
+                                        message,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+    }
+
+    private void InnitializeFields()
+    {
+        UpdateAccountSettings = findViewById(R.id.update_settings_button);
+        userName = findViewById(R.id.set_user_name);
+        userStatus = findViewById(R.id.set_profile_status);
+        userProfileImage = findViewById(R.id.set_profile_image);
+    }
+    private void sendUserToMainActivity()
+    {
+        Intent MainIntent = new Intent(SettingsActivity.this,
+                MainActivity.class);
+        MainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(MainIntent);
+        finish();
+    }
+}
