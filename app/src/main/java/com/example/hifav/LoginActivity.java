de456d0883dcd2084f5bb38fcc44b55a46908c10
Ajuogaaz <brianlinus1753@gmail.com>
2020-03-30 17:51:19 -0500

    Phone Authentication Ui

--- app/src/main/java/com/example/hifav/LoginActivity.java
+++ app/src/main/java/com/example/hifav/LoginActivity.java
@@ -54,6 +54,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
+                startActivity(phoneLoginIntent);
+            }
+        });
     }
 
     private void AllowUserToLogin()
--- app/src/main/java/com/example/hifav/PhoneLoginActivity.java
+++ app/src/main/java/com/example/hifav/PhoneLoginActivity.java
@@ -3,12 +3,36 @@ package com.example.hifav;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PhoneLoginActivity extends AppCompatActivity {
+
+    private Button SendVerificationCodeButton, VerifyButton;
+    private EditText InputPhoneNumber, InputVerificationCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_login);
+
+        SendVerificationCodeButton = findViewById(R.id.send_verification_code);
+        VerifyButton = findViewById(R.id.verify_button);
+        InputVerificationCode = findViewById(R.id.verification_code_input);
+        InputPhoneNumber = findViewById(R.id.phone_number_input);
+
+
+        SendVerificationCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SendVerificationCodeButton.setVisibility(View.INVISIBLE);
+                InputPhoneNumber.setVisibility(View.INVISIBLE);
+
+                VerifyButton.setVisibility(View.VISIBLE);
+                InputVerificationCode.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
